f8056fadd069fdbe2490a8c73056e54433a9e139
Odysseus <320873791@qq.com>
2021-10-01 11:40:35 +0800

    Update basic_task/hello/main.go

--- basic_task/hello/main.go
+++ basic_task/hello/main.go
@@ -1,13 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func addTimestampApi(c *gin.Context) {
+	var path = c.FullPath()
+	fmt.Println(path)
+	var jsonstr = c.DefaultQuery("json", "{}")
+	var obj gin.H
+	json.Unmarshal([]byte(jsonstr), &obj)
+	obj["timestamp"] = time.Now().Unix()
+	c.JSON(http.StatusOK, obj)
+}
+
+func setupRouter() *gin.Engine {
+	var r = gin.Default()
+
+	r.GET("/hello", addTimestampApi)
+
+	return r
+}
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r := setupRouter()
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080
 }
