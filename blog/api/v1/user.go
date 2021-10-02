066696a04b98ec9c331903808f5f85d9cd4f1643
Odysseus <320873791@qq.com>
2021-10-02 15:47:35 +0800

    Updated api/v1/users.go

--- blog/api/v1/user.go
+++ blog/api/v1/user.go
@@ -1,15 +1,43 @@
 package v1
 
-//注册
+import "github.com/gin-gonic/gin"
 
-//查看个人页
+//注册
+func SignIn(c *gin.Context) {
+
+}
 
 //查询用户是否已存在
+func IsUserExist(c *gin.Context) {
+
+}
+
+//查看个人页
+func GetUserPage(c *gin.Context) {
+
+}
 
 //查询单个用户
+func GetOneUser(c *gin.Context) {
+
+}
 
 //查询用户列表
+func GetUserList(c *gin.Context) {
 
-//修改个人信息
+}
 
-//注销
+//编辑个人信息
+func EditInformation(c *gin.Context) {
+
+}
+
+//修改密码
+func ChangePassword(c *gin.Context) {
+
+}
+
+//删除用户
+func DeleteUser(c *gin.Context) {
+
+}
