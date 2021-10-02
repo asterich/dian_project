8b704ed8d01cdf9ec4ed336be0771df8b4c54c29
Odysseus <320873791@qq.com>
2021-10-03 00:12:09 +0800

    Updated utils/crypto.go

--- blog/utils/crypto.go
+++ blog/utils/crypto.go
@@ -2,14 +2,14 @@ package utils
 
 import (
 	"blog/utils/errmsg"
-	"fmt"
+	"log"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func ValidatePassword(hashedPassword string, plainPassword string) (bool, errmsg.ErrCode) {
 	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword)); err != nil {
-		return false, errmsg.ERROR_PASSWORD_INCORRECT
+		return false, errmsg.ERROR
 	}
 	return true, errmsg.SUCCEED
 }
@@ -17,7 +17,7 @@ func ValidatePassword(hashedPassword string, plainPassword string) (bool, errmsg
 func GeneratePassword(plainPassword string) (string, errmsg.ErrCode) {
 	var hashedPassword, err = bcrypt.GenerateFromPassword([]byte(plainPassword), bcrypt.DefaultCost)
 	if err != nil {
-		fmt.Println("Failed to generate hashed password, err: ", err)
+		log.Println("Failed to generate hashed password, err: ", err)
 		return string(hashedPassword), errmsg.ERROR
 	}
 	return string(hashedPassword), errmsg.SUCCEED
