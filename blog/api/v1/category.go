8ffef1620a803496ce3c100fd0b1e140064d8381
Odysseus <320873791@qq.com>
2021-10-02 16:27:41 +0800

    Edit api/v1/category.go

--- blog/api/v1/category.go
+++ blog/api/v1/category.go
@@ -1,11 +1,28 @@
 package v1
 
+import "github.com/gin-gonic/gin"
+
 //创建分类
+func CreateCategory(c *gin.Context) {
+
+}
 
 //查询分类是否已存在
+func IsCategoryExist(c *gin.Context) {
+
+}
 
 //查询单个分类下的文章
+func GetArticlesUnderCategory(c *gin.Context) {
+
+}
 
 //查看分类列表
+func GetCategoryList(c *gin.Context) {
+
+}
 
 //删除分类
+func DeleteCategory(c *gin.Context) {
+
+}
