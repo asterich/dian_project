f840cadd01d9dff996b25963b8bcccbbf6dc3416
Odysseus <320873791@qq.com>
2021-10-02 16:27:23 +0800

    Edit api/v1/article.go

--- blog/api/v1/article.go
+++ blog/api/v1/article.go
@@ -1,9 +1,23 @@
 package v1
 
-//查看文章
+import "github.com/gin-gonic/gin"
 
-//上传文章
+//查看文章
+func GetArticle(c *gin.Context) {
+
+}
+
+//创建文章
+func CreateArticle(c *gin.Context) {
+
+}
 
 //修改文章
+func EditArticle(c *gin.Context) {
+
+}
 
 //删除文章
+func DeleteArticle(c *gin.Context) {
+
+}
