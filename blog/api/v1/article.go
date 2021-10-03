2ef6dc8b7fb50e80589b7b057822cd9cf1aa430f
Odysseus <320873791@qq.com>
2021-10-03 16:53:32 +0800

    完成article模块的增、删、改接口编写

--- blog/api/v1/article.go
+++ blog/api/v1/article.go
@@ -1,23 +1,55 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog/model"
+	"blog/utils/errmsg"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 //查看文章
 func GetArticle(c *gin.Context) {
-
+	var id, _ = strconv.Atoi(c.Param("id"))
+	var info, code = model.GetArticle(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"info":    info,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 //创建文章
 func CreateArticle(c *gin.Context) {
-
+	var data model.Article
+	_ = c.ShouldBindJSON(&data)
+	var code = model.CreateArticle(&data)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"data":    data,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 //修改文章
 func EditArticle(c *gin.Context) {
-
+	var id, _ = strconv.Atoi(c.Param("id"))
+	var article gin.H
+	_ = c.ShouldBindJSON(&article)
+	var code = model.EditArticle(id, article)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
 
 //删除文章
 func DeleteArticle(c *gin.Context) {
-
+	var id, _ = strconv.Atoi(c.Param("id"))
+	var code = model.DeleteArticle(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
--- blog/model/Article.go
+++ blog/model/Article.go
@@ -1,6 +1,12 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"blog/utils/errmsg"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 type Article struct {
 	gorm.Model
@@ -25,4 +31,59 @@ type Article struct {
 
 	//图片
 	Img string `gorm:"type:text;not null" json:"img"`
+}
+
+//获取文章
+func GetArticle(id int) (Article, errmsg.ErrCode) {
+	var article Article
+	var err = db.Model(&Article{}).Where("id = ?", id).First(&article).Error
+	if err != nil {
+		log.Println("Failed to get article, err: ", err)
+		return article, errmsg.ERROR
+	}
+	return article, errmsg.SUCCEED
+}
+
+//查询文章列表
+func GetArticleList(PageSize int, PageNum int) []Article {
+	var articles []Article
+	var err = db.Model(&Article{}).Limit(PageSize).Offset((PageNum - 1) * PageSize).Find(&articles).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return articles
+}
+
+//创建文章
+func CreateArticle(article *Article) errmsg.ErrCode {
+	var err = db.Create(&article).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCEED
+}
+
+//修改文章
+func EditArticle(id int, data gin.H) errmsg.ErrCode {
+	var err = db.Model(&Article{}).Where("id = ?", id).Updates(map[string]interface{}{
+		"title":       data["title"],
+		"description": data["description"],
+		"contents":    data["contents"],
+		"img":         data["img"],
+	}).Error
+	if err != nil {
+		log.Println("Failed to edit article, err: ", err.Error())
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCEED
+}
+
+//删除文章
+func DeleteArticle(id int) errmsg.ErrCode {
+	var err = db.Model(&Article{}).Where("id = ?", id).Delete(&Article{}).Error
+	if err != nil {
+		log.Println(err.Error())
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCEED
 }
