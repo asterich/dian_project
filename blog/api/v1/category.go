4da89a19ddf8ab6eb58e68d2f3c752fc3b107562
Odysseus <320873791@qq.com>
2021-10-03 15:57:18 +0800

    完成category模块的增、删、改接口编写

--- blog/api/v1/category.go
+++ blog/api/v1/category.go
@@ -1,15 +1,27 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"blog/model"
+	"blog/utils/errmsg"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
 
 //创建分类
 func CreateCategory(c *gin.Context) {
-
-}
-
-//查询分类是否已存在
-func IsCategoryExist(c *gin.Context) {
-
+	var category model.Category
+	_ = c.ShouldBindJSON(&category)
+	var code = model.IsCategoryNameUsed(category.Name)
+	if code == errmsg.SUCCEED {
+		_ = model.CreateCategory(&category)
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"status":   code,
+		"catename": category.Name,
+		"message":  errmsg.GetErrMsg(code),
+	})
 }
 
 //查询单个分类下的文章
@@ -19,10 +31,20 @@ func GetArticlesUnderCategory(c *gin.Context) {
 
 //查看分类列表
 func GetCategoryList(c *gin.Context) {
-
+	var cates = model.GetCategoryList()
+	c.JSON(http.StatusOK, gin.H{
+		"status":  errmsg.SUCCEED,
+		"data":    cates,
+		"message": errmsg.GetErrMsg(errmsg.SUCCEED),
+	})
 }
 
 //删除分类
 func DeleteCategory(c *gin.Context) {
-
+	var id, _ = strconv.Atoi(c.Param("id"))
+	var code = model.DeleteCategory(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":  code,
+		"message": errmsg.GetErrMsg(code),
+	})
 }
--- blog/model/Category.go
+++ blog/model/Category.go
@@ -1,8 +1,57 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"blog/utils/errmsg"
+	"log"
+
+	"gorm.io/gorm"
+)
 
 type Category struct {
-	gorm.Model
+	ID   uint   `gorm:"primary_key; auto_increment;" json:"id"`
 	Name string `gorm:"type:varchar(20);not null" json:"name"`
+}
+
+//新增分类
+func CreateCategory(cate *Category) errmsg.ErrCode {
+	var err = db.Create(&cate).Error
+	if err != nil {
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCEED
+}
+
+//查询分类名是否已被占用
+func IsCategoryNameUsed(catename string) errmsg.ErrCode {
+	var cate Category
+	db.Model(&Category{}).Select("id").Where("name = ?", cate.Name).First(&cate)
+	if cate.ID > 0 {
+		return errmsg.ERROR_CATEGORY_ALREADY_EXIXTS
+	}
+	return errmsg.SUCCEED
+}
+
+//查询分类列表
+func GetCategoryList() []Category {
+	var cates []Category
+	var err = db.Model(&Category{}).Find(&cates).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil
+	}
+	return cates
+}
+
+//查找分类上的文章
+func GetArticlesUnderCategory() {
+
+}
+
+//删除分类
+func DeleteCategory(id int) errmsg.ErrCode {
+	var err = db.Model(&Category{}).Where("id = ?", id).Delete(&Category{}).Error
+	if err != nil {
+		log.Println(err.Error())
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCEED
 }
